1bbc1c88ef434bab465d0cc68e5d0869a00b0c94
yourZaNoza <yourZaNoza@yandex.ru>
2025-10-04 20:36:05 +0300

    PR_4 [update, without adaptive]

--- PR_4/script.js
+++ PR_4/script.js
@@ -1,8 +1,9 @@
+// Функция фильтрации
+
 const searchInput = document.getElementById("search-input");
 const bubblesContainer = document.getElementById("bubbles-container");
 const bubbles = document.querySelectorAll(".bubble");
 
-// Функция фильтрации пузырьков
 function filterBubbles() {
   const searchTerm = searchInput.value.trim().toLowerCase();
 
@@ -17,5 +18,24 @@ function filterBubbles() {
   });
 }
 
-// Обработчик события ввода
 searchInput.addEventListener("input", filterBubbles);
+
+// Успешная подписка
+
+const emailInput = document.getElementById("email-input");
+const signUpButton = document.getElementById("sign-up-button");
+const successMessage = document.getElementById("success-message");
+
+signUpButton.addEventListener("click", () => {
+  const email = emailInput.value.trim();
+
+  if (email && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    successMessage.querySelector(
+      "p"
+    ).textContent = `Мы отправили сообщение на почту\n${email}`;
+    successMessage.style.display = "block";
+    emailInput.value = "";
+  } else {
+    alert("Пожалуйста, введите корректный email.");
+  }
+});
